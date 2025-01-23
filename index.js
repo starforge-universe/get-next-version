8e08760b2319403b8a8588073f13f8ca4cce0e4a
wilddog1979 <ferenc.kemeny79@gmail.com>
2025-01-24 00:37:41 +0100

    syntax error

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (!validLevels.includes(level)) {
   process.exit(1)
 }
 
-newVersion = (version.startsWith(v) ? 'v' : '') + semver.inc(version, level)
+newVersion = (version.startsWith('v') ? 'v' : '') + semver.inc(version, level)
 
 fs.appendFileSync(process.env.GITHUB_OUTPUT, `version=${newVersion}\n`);
 process.exit(0);
